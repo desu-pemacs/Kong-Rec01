1f1ac341b47bd62222bff9f7ca794966e91097fb
MLV <mathlusiverse@gmail.com>
2021-01-27 09:26:43 -0500

    Add files via upload

---
+++ cypher.py
@@ -0,0 +1,19 @@
+# Department of Computer and Information Sciences
+# CSCI-120 Elements of Computer Programming I
+# Skill Builder 7 - Encryption with a password
+# *******************************************************
+
+import string
+
+alphabet = string.printable
+ordinal_value = {ch: i for i, ch in enumerate(alphabet)}
+
+
+def encrypt(message, password):
+    encrypted_message = ''
+    pass  # TODO replace this line with your code.
+
+
+def decrypt(message, password):
+    decrypted_message = ''
+    pass  # TODO replace this line with your code.
